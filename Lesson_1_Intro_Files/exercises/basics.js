ecf940e9e97ab1e93f9df943897e6d2964cd8773
Marian Mocanu <gheo0174@stud.kea.dk>
2020-02-07 11:01:03 +0100

    refactoring of basic exercises

--- Lesson_1_Intro_Files/exercises/basics.js
+++ Lesson_1_Intro_Files/exercises/basics.js
@@ -3,8 +3,8 @@
 // --------------------------------------
 // Exercise 1 - Console and variables
 
-var firstName = "Anders";
-var lastName = "Latif";
+var firstName = "Anders"
+var lastName = "Latif"
 // EXERCISE
 // show in the console
 // My first name is Anders and my last name is Latif
@@ -14,15 +14,13 @@ console.log("My first name is", firstName, "and my last name is", lastName)
 // --------------------------------------
 // Exercise 2 - Numbers and Strings
 
-var year = "2019";
-var number = 1;
+var year = "2019"
+var number = 1
 
 // Add the year plus the number
 // The result should be 2020
 // You cannot touch line 1 or 2
 
-
-var sum = year + number
 console.log(Number(year) + number)
 
 // --------------------------------------
