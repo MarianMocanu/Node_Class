19e9884c9a050d9284e489fea2b8ffe9b6216e00
Marian Mocanu <gheo0174@stud.kea.dk>
2020-02-14 14:00:32 +0100

    date exercises

--- Lesson_2_First_API/app.js
+++ Lesson_2_First_API/app.js
@@ -15,8 +15,32 @@ app.get("/assignment", (req, res) => {
 });
 
 app.get("/aboutMe", (req, res) => {
-    res.send({name: "Marian", age: "26", hobby: "node.js"});
+    res.send({
+        name: "Marian", 
+        age: 26, 
+        hobby: {
+            photography: "portraits",
+            sports: "basketball",
+            music: "guitar playing"
+        }
+    });
 });
 
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+app.get("/time", (req, res) => {
+    let today = new Date();
+    let day = today.getDate();
+    
+    res.send({
+        time: today.toString(),
+        day: days[today.getDay()]
+    });
+});
 
-app.listen(3000);
+app.listen(3000, error => {
+    console.log(error);
+    if (error) {
+        console.log("Error running the server.", error);
+    }
+    console.log("Server is running on port", 3000);
+});
